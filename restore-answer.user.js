0398882a51f6af048288143d5def43dbcba4d796
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2024-03-29 12:28:38 +0100

    temp2

--- restore-answer.user.js
+++ restore-answer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Enhanced Save & Restore Textarea Input with Popup Styling and Toggle
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Adds a "Restore answer" button to display a custom styled popup for restoring textarea input, with content saved to localStorage.
 // @author       You
 // @match        https://support.yardinternet.nl/*
